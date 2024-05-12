23487c5541ef5b0f60844cc345dccce2f1cd1156
gabriel-05-dev <g.luchita@protonmail.com>
2024-05-12 14:36:10 +0100

    finished!

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -6,25 +6,32 @@ const clear_button = document.querySelector('#clear_choice');
 const info_div = document.querySelector('#displayinfo');
 const battle_button = document.createElement('button');
 const battle_div = document.querySelector('#battle');
+
 const winner_information = document.createElement('p');
+winner_information.classList.add('winner_info');
+
 const new_game_btn = document.createElement('button');
 
 let player_score = 0;
 let computer_score = 0;
 let player_choice_info = document.createElement('p');
+player_choice_info.classList.add('player_choice');
 
 //display the scores
 const score_div = document.querySelector('#display_score_information');
+score_div.classList.add('display_score');
 const display_player_score = document.createElement('p');
 const display_computer_score = document.createElement('p');
 
-
+score_div.setAttribute('style', 'display: flex; gap: 1em; justify-content: center; color:white; align-items:center;');
 
 //display the winner of the game e.t.c
 
 const winner_div = document.querySelector('#winner_section');
 const display_winner = document.createElement('p');
+display_winner.classList.add('display_style');
 
+winner_div.setAttribute('style', 'display: flex; align-items:center; justify-content: center; color:white;')
 
 function updateScore() {
   display_player_score.textContent = 'Player score : ' + player_score;
@@ -60,12 +67,14 @@ clear_button.addEventListener('click', () => {
 
   battle_button.textContent = 'Battle!';
   battle_div.appendChild(battle_button);
+  battle_div.classList.add('battlediv')
 
   battle_button.addEventListener('click', () => {
     playRound(human_choice);
   })
 
   new_game_btn.textContent= 'New Game';
+  info_div.setAttribute('style', 'display:flex; margin-top: 20px; flex-direction: column; justify-content:center; align-items:center;')
   info_div.appendChild(new_game_btn);
   new_game_btn.addEventListener('click', () => {
     computer_score = 0;
@@ -77,6 +86,7 @@ clear_button.addEventListener('click', () => {
     winner_information.textContent = '';
     updatePlayerinfo(human_choice);
     player_choice_info.textContent = '';
+    display_winner.textContent = '';
   })
 
 
@@ -108,6 +118,8 @@ function check_Winner(player_score, computer_score) {
     winner_div.appendChild(display_winner);
   }
 }
+
+
 
 
 // Winner wins at 5 points! 
